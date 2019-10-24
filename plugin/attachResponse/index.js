8d448eb38fd245601ac7478c0b6c5fab6f21dc3b
Nitin <chdnitinsharma@gmail.com>
2019-10-25 01:57:37 +0530

    rename files name

--- plugin/attachResponse/index.js
+++ plugin/attachResponse/index.js
@@ -1,13 +1,13 @@
 /* https://eslint.org/docs/rules/ */
 
-const _200 = require("./_200.js");
-const _400 = require("./_400.js");
-const _500 = require("./_500.js");
+const _2xx = require("./_2xx.js");
+const _4xx = require("./_4xx.js");
+const _5xx = require("./_5xx.js");
 
 const responseAttachToRes = res => {
-  _200(res);
-  _400(res);
-  _500(res);
+  _2xx(res);
+  _4xx(res);
+  _5xx(res);
 };
 
 module.exports = responseAttachToRes;
